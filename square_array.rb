f683e3c51ee804b400a881aa770306101ca5b155
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-08 08:49:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,12 @@
 def square_array(array)
   
   squared_numbers = []
+  
   array.each do |number|
     squared = number*number
     squared_numbers << squared
   end
+  
   return squared_numbers
     
 end
